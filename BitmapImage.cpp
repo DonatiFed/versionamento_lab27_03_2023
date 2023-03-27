660df0c4a62a752f545b52bc2b5bc05b6edb7ac8
DonatiFed <federico.donati@stud.unifi.it>
2023-03-27 17:42:21 +0200

    save function added

--- BitmapImage.cpp
+++ BitmapImage.cpp
@@ -1,5 +1,16 @@
 #include "BitmapImage.h"
+bool BitmapImage::save(std::string name) {
+    // check it is a supported image file
+    std::string fileExt = name.substr(name.find_last_of(".") + 1);
+    if (fileExt == "jpg") {
+        // load image file
+        // decompress into buffer
+        return true;
+    }
+    return false;
+}
 
+}
 BitmapImage::BitmapImage() : width(640), height(480),channels(3), depth(8) {
     buffer = new char[width * height * channels];
 }
@@ -14,6 +25,7 @@ bool BitmapImage::load(std::string name) {
     }
     return false;
 }
+
 
 BitmapImage::~BitmapImage() {
     delete[] buffer;
