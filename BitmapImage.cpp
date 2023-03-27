e19e8e6b7cc5224ab595500c7848e184362bf672
DonatiFed <federico.donati@stud.unifi.it>
2023-03-27 17:47:25 +0200

    resize function added

--- BitmapImage.cpp
+++ BitmapImage.cpp
@@ -10,7 +10,19 @@ bool BitmapImage::save(std::string name) {
     return false;
 }
 
+bool BitmapImage::resize(int width, int height) {
+    // check it is a supported image file
+    std::string fileExt = name.substr(name.find_last_of(".") + 1);
+    if (fileExt == "jpg") {
+        // load image file
+        // decompress into buffer
+        return true;
+    }
+    return false;
 }
+
+
+
 BitmapImage::BitmapImage() : width(640), height(480),channels(3), depth(8) {
     buffer = new char[width * height * channels];
 }
